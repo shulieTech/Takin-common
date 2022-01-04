26c3211d2f059d2a38d253c686c40bcbe5cd2ad3
zhaoyong <zhaoyong@shulie.io>
2022-01-04 17:21:19 +0800

    sql调整

--- takin-common-job/src/main/java/io/shulie/takin/job/job/ClearLogJob.java
+++ takin-common-job/src/main/java/io/shulie/takin/job/job/ClearLogJob.java
@@ -23,7 +23,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author 无涯
@@ -31,22 +35,32 @@ import org.springframework.stereotype.Component;
  * @date 2021/6/21 5:44 下午
  */
 @Component
-@ElasticSchedulerJob(jobName = "clearLogJob",cron = "0 0 1 * * ?",
-    description = "每天凌晨1点，清理JOB_EXECUTION_LOG、JOB_STATUS_TRACE_LOG的3天前数据")
+@ElasticSchedulerJob(jobName = "clearLogJob", cron = "0 0 1 * * ?",
+        description = "每天凌晨1点，清理JOB_EXECUTION_LOG、JOB_STATUS_TRACE_LOG的3天前数据")
 @Slf4j
 public class ClearLogJob implements SimpleJob {
     @Autowired
     private Environment environment;
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
     @Override
     public void execute(ShardingContext shardingContext) {
         String clearTime = environment.getProperty("job.clear.day.time");
-        if(StringUtils.isBlank(clearTime)) {
+        if (StringUtils.isBlank(clearTime)) {
             clearTime = "3";
         }
         log.info("清理JOB_EXECUTION_LOG、JOB_STATUS_TRACE_LOG");
-        jdbcTemplate.update("delete from JOB_EXECUTION_LOG where datediff(curdate(), START_TIME) >= " + clearTime );
-        jdbcTemplate.update("delete from JOB_STATUS_TRACE_LOG where datediff(curdate(), CREATION_TIME) >= " + clearTime);
+        String finalClearTime = clearTime;
+        jdbcTemplate.update("delete from JOB_EXECUTION_LOG where datediff(curdate(), START_TIME) >= ?", new PreparedStatementSetter() {
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, finalClearTime);
+            }
+        });
+        jdbcTemplate.update("delete from JOB_STATUS_TRACE_LOG where datediff(curdate(), CREATION_TIME) >= ?", new PreparedStatementSetter() {
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, finalClearTime);
+            }
+        });
     }
 }
