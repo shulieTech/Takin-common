5f316c33144877a80d34122a90b20a1004459d5d
zhaoyong <zhaoyong@shulie.io>
2022-01-04 16:57:13 +0800

    升级版本

--- takin-common-parent/takin-common-utils/src/main/java/io/shulie/takin/utils/log/LogHelper.java
+++ takin-common-parent/takin-common-utils/src/main/java/io/shulie/takin/utils/log/LogHelper.java
@@ -1,27 +1,26 @@
 package io.shulie.takin.utils.log;
 
-import com.sun.tools.javac.util.Log;
 import org.springframework.web.util.JavaScriptUtils;
 
 public class LogHelper {
 
     /**
-     * 转移特殊字符
-     * @param content
-     * @return
+     * 转义特殊字符
+     * @param content 内容
+     * @return 转义之后的内容
      */
     public static String converterString(String content) {
         return JavaScriptUtils.javaScriptEscape(content);
     }
 
     /**
-     *
-     * @param format
-     * @param args
-     * @return
+     * 转义特殊字符
+     * @param format 规则
+     * @param args 规则对应参数
+     * @return 转义之后的内容
      */
     public static String format(String format, Object... args){
-        String log = Log.format(format, args);
+        String log = String.format(format, args);
         return JavaScriptUtils.javaScriptEscape(log);
     }
 }
