4be5e8e94eac52a14334c73990b41611c1a3cc1c
zhaoyong <zhaoyong@shulie.io>
2022-01-05 10:49:44 +0800

    日志内容转移

--- takin-common-parent/takin-common-utils/src/main/java/io/shulie/takin/utils/log/LogHelper.java
+++ takin-common-parent/takin-common-utils/src/main/java/io/shulie/takin/utils/log/LogHelper.java
@@ -1,11 +1,13 @@
 package io.shulie.takin.utils.log;
 
+
 import org.springframework.web.util.JavaScriptUtils;
 
 public class LogHelper {
 
     /**
      * 转义特殊字符
+     *
      * @param content 内容
      * @return 转义之后的内容
      */
@@ -15,12 +17,15 @@ public class LogHelper {
 
     /**
      * 转义特殊字符
+     *
      * @param format 规则
-     * @param args 规则对应参数
+     * @param args   规则对应参数
      * @return 转义之后的内容
      */
-    public static String format(String format, Object... args){
+    public static String format(String format, Object... args) {
         String log = String.format(format, args);
         return JavaScriptUtils.javaScriptEscape(log);
     }
+
+
 }
