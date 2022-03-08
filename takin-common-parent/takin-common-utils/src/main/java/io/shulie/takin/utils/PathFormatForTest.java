458727a35e7cdbd899f453395381c520984f8789
pnz.zhao <zhaopnz@126.com>
2022-03-08 10:24:49 +0800

    文件路径配置支持本地测试

---
+++ takin-common-parent/takin-common-utils/src/main/java/io/shulie/takin/utils/PathFormatForTest.java
@@ -0,0 +1,34 @@
+package io.shulie.takin.utils;
+
+/**
+ * @author pnz.zhao
+ * @Description:
+ * @date 2022/3/8 10:10
+ */
+public class PathFormatForTest {
+    /**
+     * 本地文件目录配置，改动点太多；而且有些在文件中，有些在数据库中配置，好乱
+     * 为了便于本地测试，暂用路径重新格式化方式改动
+     * @param path
+     * @return
+     */
+    public static String format(String path) {
+        boolean isLocEnv;
+        String os = System.getProperties().getProperty("os.name").toLowerCase();
+        if (os.contains("win") || os.contains("mac")) {
+            isLocEnv = true;
+        } else {
+            isLocEnv = false;
+        }
+        if(isLocEnv) {
+            if (!path.replace("//", "/").startsWith("/Users/"+System.getProperty("user.name")+"/")) {
+                path = "/Users/"+System.getProperty("user.name")+"/" + path.replace("/nfs_dir", "");
+            }
+        }
+        return path;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(format("a"));
+    }
+}
